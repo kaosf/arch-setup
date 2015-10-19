779965a775299ab2b7e5f0d61c98b4b64bc13a83
ka <ka.kaosf@gmail.com>
2015-10-19 16:44:02 +0900

    Create unsecure-root-setup.sh

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ curl https://raw.githubusercontent.com/kaosf/arch-setup/master/go-setup.sh | sh
 curl https://raw.githubusercontent.com/kaosf/arch-setup/master/vim-setup.sh | sh
 ```
 
+```sh
+curl https://raw.githubusercontent.com/kaosf/arch-setup/master/unsecure-root-setup.sh | sh $USERNAME
+```
+
 ## License
 
 [![Public Domain](http://i.creativecommons.org/p/mark/1.0/88x31.png)](http://creativecommons.org/publicdomain/mark/1.0/ "license")
---
+++ unsecure-root-setup.sh
@@ -0,0 +1,12 @@
+if [ $# -lt 1 ]; then exit 1; fi
+NAME=$1
+pacman --noconfirm -S zsh
+cp -r /etc/skel /home/$NAME
+useradd $NAME -s /usr/bin/zsh
+echo "${NAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+mkdir /home/${NAME}/.ssh
+cp .ssh/authorized_keys /home/${NAME}/.ssh
+chmod 600 /home/${NAME}/.ssh/authorized_keys
+chown ${NAME}:${NAME} -R /home/${NAME}
+sed 's/PermitRootLogin yes/PermitRootLogin no/' -i /etc/ssh/sshd_config
+systemctl restart sshd
