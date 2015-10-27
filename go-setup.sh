2836766e3e27253c02fac9f03069fde9af88357b
ka <ka.kaosf@gmail.com>
2015-10-27 16:22:06 +0900

    Fix go-setup.sh

--- go-setup.sh
+++ go-setup.sh
@@ -1,9 +1,11 @@
 sudo pacman --noconfirm -S go
 mkdir -p ~/.go
-export GOROOT=$HOME/.go
-export PATH=$GOROOT/bin:$PATH
-echo 'export GOROOT=$HOME/.go'             >> $HOME/.bash_profile
-echo 'export PATH=$GOROOT/bin:$PATH'       >> $HOME/.bash_profile
+export GOPATH=$HOME/.gocode
+export PATH=$HOME/.gocode/bin:$PATH
 echo 'export GOPATH=$HOME/.gocode'         >> $HOME/.zprofile
 echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zprofile
+echo 'export GOPATH=$HOME/.gocode'         >> $HOME/.zshenv
+echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zshenv
+echo 'export GOPATH=$HOME/.gocode'         >> $HOME/.bash_profile
+echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.bash_profile
 go get github.com/peco/peco/cmd/peco
