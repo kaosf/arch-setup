6d2e4aac329f220a2561d582cce13b8e87fb9573
ka <ka.kaosf@gmail.com>
2015-10-27 15:26:41 +0900

    Remove "exec $SHELL" and add "export PATH=..."

--- go-setup.sh
+++ go-setup.sh
@@ -1,8 +1,9 @@
 sudo pacman --noconfirm -S go
 mkdir -p ~/.go
+export GOROOT=$HOME/.go
+export PATH=$GOROOT/bin:$PATH
 echo 'export GOROOT=$HOME/.go'             >> $HOME/.bash_profile
 echo 'export PATH=$GOROOT/bin:$PATH'       >> $HOME/.bash_profile
 echo 'export GOPATH=$HOME/.gocode'         >> $HOME/.zprofile
 echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zprofile
-exec $SHELL
 go get github.com/peco/peco/cmd/peco
