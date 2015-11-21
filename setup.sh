55cc3526143a4ac157bdb820ec5ed03c9bcbbb71
ka <ka.kaosf@gmail.com>
2015-11-21 17:12:46 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
-sudo pacman -Syy
-sudo pacman --noconfirm -S git tmux tig
+cat <<EOF | sudo tee -a /etc/pacman.conf
+[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/\$arch
+EOF
+sudo pacman --noconfirm --sync --refresh yaourt
+
+yaourt --noconfirm -Syua
+yaourt --noconfirm -S git tmux tig
 mkdir -p $HOME/local/bin
 echo 'export PATH=$HOME/local/bin:$PATH' >> $HOME/.zprofile
