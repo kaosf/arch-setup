7b5eed32b04261c9009b11b2f88b71b49053d284
ka <ka.kaosf@gmail.com>
2015-11-21 17:24:43 +0900

    Enable to run "setup.sh" any number of times

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
-cat <<EOF | sudo tee -a /etc/pacman.conf
+if cat /etc/pacman.conf | grep archlinuxfr > /dev/null 2>&1; then
+  echo '# Configurations for yaourt is OK'
+else
+  cat <<EOF | sudo tee -a /etc/pacman.conf
 [archlinuxfr]
 SigLevel = Never
 Server = http://repo.archlinux.fr/\$arch
 EOF
-sudo pacman --noconfirm --sync --refresh yaourt
+  sudo pacman --noconfirm --sync --refresh yaourt
+fi
 
 yaourt --noconfirm -Syua
 yaourt --noconfirm -S git tmux tig
+
 mkdir -p $HOME/local/bin
-echo 'export PATH=$HOME/local/bin:$PATH' >> $HOME/.zprofile
+if echo $PATH | grep "$HOME/local/bin" > /dev/null 2>&1; then
+  echo '# Local PATH is OK'
+else
+  echo 'export PATH=$HOME/local/bin:$PATH' >> $HOME/.zprofile
+  echo 'export PATH=$HOME/local/bin:$PATH' >> $HOME/.zshenv
+  echo 'export PATH=$HOME/local/bin:$PATH' >> $HOME/.profile
+  echo 'export PATH=$HOME/local/bin:$PATH' >> $HOME/.bash_profile
+fi
