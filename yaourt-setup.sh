c1f50f6fcf8fa82b73196d883b7333acd6fac1ef
ka <ka.kaosf@gmail.com>
2015-11-21 17:09:35 +0900

    Create yaourt-setup.sh

---
+++ yaourt-setup.sh
@@ -0,0 +1,7 @@
+cat <<EOF | sudo tee -a /etc/pacman.conf
+[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/\$arch
+EOF
+sudo pacman --sync --refresh yaourt
+# ref. https://wiki.archlinuxjp.org/index.php/Yaourt
