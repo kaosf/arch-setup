c5a0570146ae44b26f58341fd38d41b0eb38da85
ka <ka.kaosf@gmail.com>
2015-12-17 01:56:13 +0900

    Add privilege to myself

--- postgresql-setup.sh
+++ postgresql-setup.sh
@@ -1,7 +1,10 @@
 yaourt -S postgresql
 
+USERNAME=`whoami`
+
 sudo -i -u postgres
 initdb --locale en_US.UTF-8 -E UTF8 -D '/var/lib/postgres/data'
+postgres -c "createuser ${USERNAME} -d -l"
 exit
 
 sudo systemctl start postgresql
