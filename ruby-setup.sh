3abff1c6c95ad28eec80f5de123d92a93246571c
ka <ka.kaosf@gmail.com>
2016-01-03 03:12:21 +0900

    Upgrade Ruby version to 2.3.0

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -4,9 +4,9 @@ if cat $HOME/.bash_profile | grep GEM_HOME > /dev/null 2>&1; then
   echo '# $GEM_HOME and $PATH are already set.'
 else
   echo 'export GEM_HOME=$HOME/.gem'                 >> $HOME/.zshenv
-  echo 'export PATH=$GEM_HOME/ruby/2.2.0/bin:$PATH' >> $HOME/.zshenv
+  echo 'export PATH=$GEM_HOME/ruby/2.3.0/bin:$PATH' >> $HOME/.zshenv
   echo 'export GEM_HOME=$HOME/.gem'                 >> $HOME/.zprofile
-  echo 'export PATH=$GEM_HOME/ruby/2.2.0/bin:$PATH' >> $HOME/.zprofile
+  echo 'export PATH=$GEM_HOME/ruby/2.3.0/bin:$PATH' >> $HOME/.zprofile
   echo 'export GEM_HOME=$HOME/.gem'                 >> $HOME/.bash_profile
-  echo 'export PATH=$GEM_HOME/ruby/2.2.0/bin:$PATH' >> $HOME/.bash_profile
+  echo 'export PATH=$GEM_HOME/ruby/2.3.0/bin:$PATH' >> $HOME/.bash_profile
 fi
