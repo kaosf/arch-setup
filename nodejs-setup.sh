780aaa3c56a219beafc3c2e99c50ea631717099a
ka <ka.kaosf@gmail.com>
2016-01-20 19:49:28 +0900

    Upgrade Node.js version to 5.4.1

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -3,5 +3,5 @@ echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zprofile
 echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zshenv
 echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.bash_profile
 exec $SHELL
-nodebrew install-binary 5.1.1
+nodebrew install-binary 5.4.1
 nodebrew use latest
