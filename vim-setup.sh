d3257fa758cb94e022b35150f011dca00f3bf3ca
ka <ka.kaosf@gmail.com>
2016-03-02 00:33:36 +0900

    Replace from NeoBundle to dein.vim

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,7 +1,6 @@
 sudo pacman --noconfirm -S git vim
-[ -d $HOME/.vim/bundle ] || mkdir -p $HOME/.vim/bundle
-if [ ! -d $HOME/.vim/bundle/neobundle.vim ]; then
-  git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+if [ ! -d $HOME/.vim/dein ]; then
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s $HOME/.vim/dein
 fi
 if [ ! -d $HOME/.vim/ka-vim-snippets ]; then
   git clone git://github.com/kaosf/ka-vim-snippets.git $HOME/.vim/ka-vim-snippets
