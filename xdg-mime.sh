5cb59e345c16877f27e972801214de4f1967e39a
ka <ka.kaosf@gmail.com>
2016-08-18 16:49:57 +0900

    Create xdg-mime.sh

---
+++ xdg-mime.sh
@@ -0,0 +1,22 @@
+# # Default
+# xdg-mime query default text/html
+# #=> userapp-Firefox-??????.desktop
+# xdg-mime query default x-scheme-handler/http
+# #=> userapp-Firefox-??????.desktop
+# xdg-mime query default x-scheme-handler/https
+# #=> userapp-Firefox-??????.desktop
+
+# # Set
+# xdg-mime default firefox.desktop text/html
+# xdg-mime default firefox.desktop x-scheme-handler/http
+# xdg-mime default firefox.desktop x-scheme-handler/https
+
+# # Set 2
+# yaourt -S midori
+# xdg-mime default midori.desktop text/html
+# xdg-mime default midori.desktop x-scheme-handler/http
+# xdg-mime default midori.desktop x-scheme-handler/https
+
+# ref.
+# http://cocu.hatenablog.com/entry/2014/09/20/224028
+# http://linux.die.net/man/1/xdg-mime
