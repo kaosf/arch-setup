5440f11aea476d361f783dcded121b7c01f51393
ka <ka.kaosf@gmail.com>
2016-11-05 16:23:46 +0900

    Update postgresql-setup.sh

--- postgresql-setup.sh
+++ postgresql-setup.sh
@@ -1,11 +1,13 @@
-yaourt -S postgresql
+sudo pacman -S postgresql
 
 sudo -i -u postgres
 initdb --locale en_US.UTF-8 -E UTF8 -D '/var/lib/postgres/data'
-sudo systemctl start postgresql
 exit
 
-USERNAME=`whoami`
+sudo systemctl start postgresql.service
+
+# "<YOUR_USERNAME>" must be modified to your real user name.
+# (the result of "whoami" command output)
 sudo -i -u postgres
-createuser ${USERNAME} -d -l
+createuser <YOUR_USERNAME> -d -l
 exit
