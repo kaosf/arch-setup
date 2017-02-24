16c603e83f3b5cb38055336b646f51cd8590176b
ka <ka.kaosf@gmail.com>
2017-02-24 13:39:29 +0900

    Update rbenv-setup.sh

--- rbenv-setup.sh
+++ rbenv-setup.sh
@@ -1,14 +1,14 @@
-yaourt -S rbenv ruby-build
-mkdir -p $HOME/.rbenv/plugins
-git clone https://github.com/ianheggie/rbenv-binstubs.git $HOME/.rbenv/plugins/rbenv-binstubs
-echo 'eval "$(rbenv init -)"' >> ~/.zprofile
-echo 'eval "$(rbenv init -)"' >> ~/.zshenv
-echo 'eval "$(rbenv init -)"' >> ~/.profile
-exec $SHELL
+# The case of using rbenv
+sudo pacman --noconfirm -S rbenv ruby-build
+# Remove GEM_HOME and add "unset GEM_HOME" until next reboot.
+echo 'eval "$(rbenv init -)"' >> $HOME/.zshenv
+echo 'eval "$(rbenv init -)"' >> $HOME/.zprofile
+echo 'eval "$(rbenv init -)"' >> $HOME/.bash_profile
 
-# rbenv install 1.9.3-p551
-# rbenv local 1.9.3-p551
+# rbenv install 2.3.3
+# rbenv local 2.3.3
 # gem install bundler
+# rbenv rehash
 
 # ref.
 #   https://wiki.archlinux.org/index.php/Ruby
