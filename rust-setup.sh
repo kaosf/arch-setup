1f5671e67bdc1f5f026621b9d47240963e141e04
ka <ka.kaosf@gmail.com>
2017-02-26 11:07:25 +0900

    Add some rustup command examples

--- rust-setup.sh
+++ rust-setup.sh
@@ -5,3 +5,6 @@ rustup default stable
 # ref.
 #   http://keens.github.io/blog/2016/06/12/multirustgahisuishouninattayounanoderustupniikousuru/
 #   https://wiki.archlinuxjp.org/index.php/Rust#Rustup_.E3.81.AB.E3.82.88.E3.82.8B.E5.85.AC.E5.BC.8F.E3.81.AE.E3.82.A4.E3.83.B3.E3.82.B9.E3.83.88.E3.83.BC.E3.83.AB.E6.96.B9.E6.B3.95
+
+rustup target add x86_64-pc-windows-msvc
+rustup target remove x86_64-pc-windows-msvc
