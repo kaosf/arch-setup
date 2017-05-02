60edf796c15c32f82903839b5c4890bcd0d28859
ka <ka.kaosf@gmail.com>
2017-05-02 10:28:45 +0900

    Create tmux-setup.sh for tmux version 2.3

---
+++ tmux-setup.sh
@@ -0,0 +1,10 @@
+VERSION=2.3
+
+#sudo pacman --noconfirm -S ????
+
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar xzf tmux-${VERSION}.tar.gz
+mkdir -p $HOME/local
+./configure --prefix=$HOME/local
+make
+make install
