6caa72e2f1da9f4ffa3b8430491eabd4f336b7a8
ka <ka.kaosf@gmail.com>
2017-05-08 16:08:10 +0900

    Fix tmux-setup.sh by adding "cd" command

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -5,6 +5,7 @@ VERSION=2.3
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 tar xzf tmux-${VERSION}.tar.gz
 mkdir -p $HOME/local
+cd tmux-${VERSION}
 ./configure --prefix=$HOME/local
 make
 make install
