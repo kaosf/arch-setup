050520dd990200e97464d08ac58399a6e2822018
ka <ka.kaosf@gmail.com>
2017-12-29 11:26:07 +0900

    Fix nodejs-setup.sh

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -3,5 +3,5 @@ echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zprofile
 echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zshenv
 echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.bash_profile
 exec $SHELL
-nodebrew install-binary 8.2.1
+nodebrew install-binary latest
 nodebrew use latest
