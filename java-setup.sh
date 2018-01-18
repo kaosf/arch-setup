c1f30901b728577b3412607fc447aa83b148c70f
ka <ka.kaosf@gmail.com>
2018-01-19 01:26:00 +0900

    Create java-setup.sh

---
+++ java-setup.sh
@@ -0,0 +1,8 @@
+sudo pacman -S jdk9-openjdk
+sudo pacman -S jdk8-openjdk
+sudo pacman -S jdk7-openjdk
+
+# Change Java version
+archlinux-java help
+archlinux-java status # To check <JAVA_ENV>
+sudo archlinux-java java-8-openjd
