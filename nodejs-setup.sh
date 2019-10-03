d311ffd27befac57a0ef6274e484153eed45475a
ka <ka.kaosf@gmail.com>
2019-10-04 04:23:44 +0900

    Update Node.js setup

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -1,7 +1,20 @@
-curl -L git.io/nodebrew | perl - setup
-echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zprofile
-echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zshenv
-echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.bash_profile
-exec $SHELL
-nodebrew install-binary latest
-nodebrew use latest
+# Install anyenv at first
+
+anyenv install nodenv
+
+# Install Yarn
+# ref. https://github.com/pine/nodenv-yarn-install
+mkdir -p "$(nodenv root)/plugins"
+git clone https://github.com/pine/nodenv-yarn-install.git "$(nodenv root)/plugins/nodenv-yarn-install"
+
+nodenv install 10.16.3
+nodenv install 12.11.1
+
+# Otherwise...
+
+sudo pacman -S nodejs npm yarn
+
+# System wide Node.js switching
+sudo npm i -g n
+sudo n 10.16.3
+sudo n 12.11.1
