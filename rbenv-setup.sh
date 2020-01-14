bebe9ffc4c1a137e6b5cc5a16fb2b8349ef714b3
ka <ka.kaosf@gmail.com>
2020-01-14 13:51:53 +0900

    Install rbenv with anyenv

--- rbenv-setup.sh
+++ rbenv-setup.sh
@@ -1,15 +1,1 @@
-# The case of using rbenv
-yaourt --noconfirm -S rbenv ruby-build
-# Remove GEM_HOME and add "unset GEM_HOME" until next reboot.
-echo 'eval "$(rbenv init -)"' >> $HOME/.zshenv
-echo 'eval "$(rbenv init -)"' >> $HOME/.zprofile
-echo 'eval "$(rbenv init -)"' >> $HOME/.bash_profile
-
-# rbenv install 2.3.3
-# rbenv local 2.3.3
-# gem install bundler
-# rbenv rehash
-
-# ref.
-#   https://wiki.archlinux.org/index.php/Ruby
-#   https://wiki.archlinux.org/index.php/Rbenv
+anyenv install rbenv
