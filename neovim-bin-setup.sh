769e2169ff608aa72541bbab0eeb98a2d82e4517
ka <ka.kaosf@gmail.com>
2020-10-16 16:40:02 +0900

    Add neovim-bin-setup.sh

---
+++ neovim-bin-setup.sh
@@ -0,0 +1,6 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+
+# ref. https://neovim.io/
+# ref. https://github.com/neovim/neovim/wiki/Installing-Neovim
