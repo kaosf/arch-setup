c2dab8cc2cc5884c6c6ce4f4c9b48e3c93646f7f
ka <ka.kaosf@gmail.com>
2021-12-24 18:17:38 +0900

    Install heroku command with AUR

--- heroku-setup.sh
+++ heroku-setup.sh
@@ -1,10 +1,2 @@
-wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x86.tar.gz -O heroku.tar.gz
-tar -xvzf heroku.tar.gz
-mkdir -p /usr/local/lib /usr/local/bin
-sudo mv heroku-cli-v*-linux-x86 /usr/local/lib/heroku
-sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
-# ref. https://devcenter.heroku.com/articles/heroku-cli#standalone
-
-# or
-npm install -g heroku-cli
-# ref. https://devcenter.heroku.com/articles/heroku-cli#npm
+# ref. https://devcenter.heroku.com/articles/heroku-cli#arch-linux
+yay -S heroku-cli-bin
