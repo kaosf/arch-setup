2cb1dd759591d4dd06c6614df24aae22fb423f7b
ka <ka.kaosf@gmail.com>
2023-03-17 20:52:54 +0900

    Update Vim setup

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,10 +1,9 @@
-sudo pacman --noconfirm -S git vim
-if [ ! -d $HOME/.vim/dein ]; then
-  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s $HOME/.vim/dein
-fi
+sudo pacman --noconfirm -S git neovim gvim python
+
 if [ ! -d $HOME/.vim/ka-vim-snippets ]; then
+  mkdir -p $HOME/.vim
   git clone git://github.com/kaosf/ka-vim-snippets.git $HOME/.vim/ka-vim-snippets
 fi
 
 # Install Python 3 "neovim" package
-pip3.10 install --user neovim
+pip install --user neovim
